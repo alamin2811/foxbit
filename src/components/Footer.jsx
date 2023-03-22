81f3037d0f2aa92f52bf2fd5d2095683feaa3f71
Al Amin Islam <alaminbinmonsur@gmail.com>
2023-03-23 02:03:14 +0600

    modifed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ import telegram from "../../images/telegram.svg";
 
 const Footer = () => (
   <div className="w-full flex md:justify-center justify-between items-center flex-col p-4 gradient-bg-footer">
-    <div className="w-full flex sm:flex-row flex-col justify-between sm:w-[90%] items-center my-4">
+    <div className="w-full flex flex-row justify-between sm:w-[90%] items-center my-4">
       <div className="flex flex-[0.5] justify-start items-center">
         <img src={logo} alt="logo" className="w-32" />
       </div>
