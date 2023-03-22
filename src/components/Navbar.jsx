4d9f599311f41d46fe171c5d26a07dd8d09a76d1
Al Amin Islam <alaminbinmonsur@gmail.com>
2023-03-23 02:26:04 +0600

    modified

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
       <div className="md:flex-[0.6] xl:flex-[0.5] flex-initial justify-center items-center">
         <img src={logo} alt="logo" className="w-32 cursor-pointer" />
       </div>
-      <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
+      <ul className="text-white flex list-none flex-row justify-between items-center flex-initial">
         <li><a href="https://twitter.com/clickFoxBit"><img className="text-center mx-2 cursor-pointer" src={twiter} alt="icon" /></a></li>
         <li><a href="https://t.me/clickFoxBit"><img className="text-center mx-2 cursor-pointer" src={telegram} alt="icon" /></a></li>
         {/* <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
